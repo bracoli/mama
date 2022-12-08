ed6868ab0c98354c3e147dd065af1c9709414cbe
bracoli <108661415+bracoli@users.noreply.github.com>
2022-12-08 19:11:03 +0800

    Add files via upload

---
+++ update/getupdate.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# My Telegram : https://t.me/Akbar218
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+
+clear
+lizupdate="raw.githubusercontent.com/lizsvr/cfnfree/main/update"
+# change direct
+cd /usr/bin
+# remove file
+rm menu
+rm -rf menu
+# Download update
+wget -O menu "https://${lizupdate}/menu.sh"
+wget -O maddssh "https://${lizupdate}/maddssh.sh"
+wget -O mbackup "https://${lizupdate}/mbackup.sh"
+wget -O maddxray "https://${lizupdate}/maddxray.sh"
+wget -O msetting "https://${lizupdate}/msetting.sh"
+wget -O start-menu "https://${lizupdate}/start-menu.sh"
+# change Permission
+chmod +x menu
+chmod +x maddssh
+chmod +x maddxray
+chmod +x mbackup
+chmod +x msetting
+chmod +x start-menu
+#change direct
+cd /root
+# clear
+clear
+echo -e "Succes Update Menu"
+sleep 3
